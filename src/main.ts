2d31f05b97242f0a9b3880cf75ce6bea0332245b
anasriyaz <anas.riyaz@pickrr.com>
2022-12-21 19:39:28 +0530

    Update vercel file

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-    methods: '*'
-  });
-
   await app.listen(process.env.PORT || 3001);
 }
 
