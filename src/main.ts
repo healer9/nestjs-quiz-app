442d5e7c758f94ee9344f2ea699a00c63a8c2ad7
anasriyaz <anas.riyaz@pickrr.com>
2022-12-21 17:27:32 +0530

    Update CORS config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  
+  app.enableCors({
+    allowedHeaders: ['content-type', 'auth-token'],
+    origin: 'https://nextjs-quiz-app-seven.vercel.app',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 
