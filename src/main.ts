991610de983423ad0c7fa462f2a7186d31b2fa1d
anasriyaz <anas.riyaz@pickrr.com>
2022-12-21 18:55:49 +0530

    remove CORS handling

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   
-  app.enableCors({
-    allowedHeaders: ['content-type', 'auth-token'],
-    origin: ['https://nextjs-quiz-app-seven.vercel.app'],
-    methods: ['POST', 'PUT', 'DELETE', 'GET']
-  });
+  // app.enableCors({
+  //   allowedHeaders: ['content-type', 'auth-token'],
+  //   origin: ['https://nextjs-quiz-app-seven.vercel.app'],
+  //   methods: ['POST', 'PUT', 'DELETE', 'GET']
+  // });
 
   await app.listen(process.env.PORT || 3000);
 }
